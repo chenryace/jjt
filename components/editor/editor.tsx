764471af8e886e5c385037fdac9b9d40e738d454
chenryace <78716224+chenryace@users.noreply.github.com>
2025-04-23 08:58:33 +0800

    Update editor.tsx

--- components/editor/editor.tsx
+++ components/editor/editor.tsx
@@ -1,6 +1,6 @@
 import { FC, useEffect, useState, useCallback, KeyboardEvent as ReactKeyboardEvent, useRef } from 'react';
 import { use100vh } from 'react-div-100vh';
-import MarkdownEditor, { Props } from '@notea/rich-markdown-editor';
+import { default as MarkdownEditor, Props } from '@notea/rich-markdown-editor';
 import { useEditorTheme } from './theme';
 import useMounted from 'libs/web/hooks/use-mounted';
 import Tooltip from './tooltip';
